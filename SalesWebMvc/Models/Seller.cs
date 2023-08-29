5daa4dbe79eab5a16bd03a50a800c5596cff1cde
Hugo Machado <hugopimentamarinho@gmail.com>
2023-08-28 22:12:24 -0300

    SeedingService

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -14,7 +14,7 @@ namespace SalesWebMvc.Models
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-        public Seller(int id, string name, DateTime birthDate, string email, double baseSalary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
